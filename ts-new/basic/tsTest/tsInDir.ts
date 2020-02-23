894a489cfc203bae6eec8583cd2f9d0dcfbcc1b8
slybootslion <slybootslion@vip.qq.com>
2020-02-23 12:56:24 +0800

    2-10end

--- ts-new/basic/tsTest/tsInDir.ts
+++ ts-new/basic/tsTest/tsInDir.ts
@@ -1,5 +1,9 @@
-const add: ({a, b}: { a: number, b: number }) => number = ({a, b}) => {
+/*const add: ({a, b}: { a: number, b: number }) => number = ({a, b}) => {
     return a + b;
+}*/
+
+const add = ({a, b}: { a: number, b: number }) => {
+    return a + b
 }
 
 export {
