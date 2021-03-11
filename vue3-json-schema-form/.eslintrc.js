b4faf3a910edcbc46a7c6c46cb963baad9c3be4f
slybootslion <slybootslion@vip.qq.com>
2021-03-11 13:32:09 +0800

    2021-03-11 13:32:09

--- vue3-json-schema-form/.eslintrc.js
+++ vue3-json-schema-form/.eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "@typescript-eslint/no-use-befor-define": "off",
+    "@typescript-eslint/no-use-before-define": "off",
     "no-prototype-builtins": "off",
     "@typescript-eslint/no-explicit-any": "off"
   },
