1f0a3fe74335ec49cd943ff654bc1c30814255f4
SlybootsLion <slybootslion@vip.qq.com>
2022-01-16 18:07:59 +0800

    chore:改变setSearchParams的实现方式

--- jira/src/screens/project-list/utils.ts
+++ jira/src/screens/project-list/utils.ts
@@ -1,4 +1,4 @@
-import { useUrlQueryParam } from "../../utils/url";
+import { useSetUrlSearchParam, useUrlQueryParam } from "../../utils/url";
 import { useMemo } from "react";
 import { useProject } from "../../utils/project";
 
@@ -21,10 +21,8 @@ export const useProjectModal = () => {
   const {data: editingProject, isLoading} = useProject(+editingProjectId)
 
   const open = () => setProjectCreate({projectCreate: true})
-  const close = () => {
-    setProjectCreate({projectCreate: undefined})
-    setEditingProjectId({editingProjectId: undefined})
-  }
+  const setUrlSearchParam = useSetUrlSearchParam()
+  const close = () => setUrlSearchParam({projectCreate: undefined, editingProjectId: undefined})
 
   const startEdit = (id: number) => setEditingProjectId({editingProjectId: id})
 
--- jira/src/utils/url.ts
+++ jira/src/utils/url.ts
@@ -3,7 +3,8 @@ import { useMemo } from "react";
 import { cleanObject } from "./index";
 
 export const useUrlQueryParam = <K extends string> (keys: K[]) => {
-  const [searchParams, setSearchParam] = useSearchParams()
+  const [searchParams] = useSearchParams()
+  const setSearchParam = useSetUrlSearchParam()
   return [
     useMemo(
       () => keys.reduce((prev, key) => {
@@ -13,8 +14,18 @@ export const useUrlQueryParam = <K extends string> (keys: K[]) => {
       [searchParams]
     ),
     (params: Partial<{ [key in K]: unknown }>) => {
-      const o = cleanObject({...Object.fromEntries(searchParams), ...params}) as URLSearchParamsInit
-      return setSearchParam(o)
+      return setSearchParam(params)
     }
   ] as const
+}
+
+export const useSetUrlSearchParam = () => {
+  const [searchParams, setSearchParam] = useSearchParams()
+  return (params: { [key in string]: unknown }) => {
+    const o = cleanObject({
+      ...Object.fromEntries(searchParams),
+      ...params,
+    }) as URLSearchParamsInit
+    return setSearchParam(o)
+  }
 }
